696dd7042dbd52effc87528ad81c8551d9544ce5
IlluzionzDev <jaminstratford@gmail.com>
2021-12-07 14:20:22 +1300

    Really basic version

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,37 @@
-<div class="container">
-    <h1>Bazaar Flipper</h1>
-    <a href="#"><button class="btn btn-solid">Button</button></a>
-    <a href="#"><button class="btn btn-outline">Button</button></a>
+<script>
+    // Basic calculations
+    let normalBuyPrice = 0.0
+    let enchantedSellPrice = 0.0
+    let initalPurchaseAmount = 1
+
+    $: baseProfit = enchantedSellPrice - normalBuyPrice
+    $: profitPerItem = baseProfit / 160
+    $: totalProfit = (profitPerItem * initalPurchaseAmount) - (normalBuyPrice * initalPurchaseAmount);
+</script>
+
+<div class="container_center">
+    <div class="app">
+        <h1>Bazaar Flipper</h1>
+        
+        <p>Normal Buy Price</p>
+        <input bind:value={normalBuyPrice}>
+
+        <p>Enchanted Sell Price</p>
+        <input bind:value={enchantedSellPrice}>
+
+        <p>Amount Bought of Normal</p>
+        <input bind:value={initalPurchaseAmount}>
+
+        <p>Profit Per Item: {profitPerItem}</p>
+        <p>Total Profit: {totalProfit}</p>
+    </div>
 </div>
 
 <style lang="scss">
 	@import '../css/colors.scss';
+
+    .app {
+        margin: auto;
+        text-align: center;
+    }
 </style>
