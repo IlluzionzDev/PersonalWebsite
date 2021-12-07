896886cc455c3eef667258df5964df13d05ebc26
IlluzionzDev <jaminstratford@gmail.com>
2021-12-07 14:30:17 +1300

    Tax rate

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,10 +3,11 @@
     let normalBuyPrice = 0.0
     let enchantedSellPrice = 0.0
     let initalPurchaseAmount = 1
+    let taxRate = 1.2
 
     $: baseProfit = enchantedSellPrice - normalBuyPrice
     $: profitPerItem = baseProfit / 160
-    $: totalProfit = (profitPerItem * initalPurchaseAmount) - (normalBuyPrice * initalPurchaseAmount);
+    $: totalProfit = ((profitPerItem * initalPurchaseAmount) - (normalBuyPrice * initalPurchaseAmount)) * (1 - (taxRate / 100));
 </script>
 
 <div class="container_center">
@@ -21,6 +22,9 @@
 
         <p>Amount Bought of Normal</p>
         <input bind:value={initalPurchaseAmount}>
+
+        <p>Tax Rate</p>
+        <input bind:value={taxRate}>
 
         <p>Profit Per Item: {profitPerItem}</p>
         <p>Total Profit: {totalProfit}</p>
