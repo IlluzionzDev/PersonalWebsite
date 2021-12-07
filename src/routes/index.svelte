88be0ca55b7cb6908e38d74b8e2ba9ed60cfdf90
IlluzionzDev <jaminstratford@gmail.com>
2021-12-07 20:03:57 +1300

    More working for enchanted items

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,33 +1,62 @@
-<script>
-    // Basic calculations
-    let normalBuyPrice = 0.0
-    let enchantedSellPrice = 0.0
-    let initalPurchaseAmount = 1
-    let taxRate = 1.2
+<script context="module">
+    // Load Bazaar Items
+    export async function load({ status, error }) {
+        let products;
+        let items;
 
-    $: baseProfit = enchantedSellPrice - normalBuyPrice
-    $: profitPerItem = baseProfit / 160
-    $: totalProfit = ((profitPerItem * initalPurchaseAmount) - (normalBuyPrice * initalPurchaseAmount)) * (1 - (taxRate / 100));
+        await fetch(`https://api.hypixel.net/skyblock/bazaar`)
+        .then(r => r.json())
+        .then(data => {
+            products = data
+        });
+
+        await fetch(`https://api.hypixel.net/resources/skyblock/items`)
+        .then(r => r.json())
+        .then(data => {
+            items = data
+        });
+
+		return {
+			props: { products, items }
+		};
+	}
+</script>
+
+<script lang="ts">
+    import BazaarItem from "$lib/BazaarItem.svelte";
+
+    // Product object
+	export let products;
+    export let items;
+
+    // Map of names
+    let productMap = new Map<string, string>()
+    let productName = 'Cobblestone'
+
+    items.items.forEach(element => {
+        productMap[element.name] = element.id
+    });
+
+    // Product object
+    let product = products.products[productMap[productName]] != null ? products.products[productMap[productName]] : products.products.COBBLESTONE
+
+    function updateProduct() {
+        product = products.products[productMap[productName]] != null ? products.products[productMap[productName]] : products.products.COBBLESTONE
+    }
 </script>
 
 <div class="container_center">
     <div class="app">
         <h1>Bazaar Flipper</h1>
         
-        <p>Normal Buy Price</p>
-        <input bind:value={normalBuyPrice}>
+        <h2>Item</h2>
+        <input bind:value={productName}>
 
-        <p>Enchanted Sell Price</p>
-        <input bind:value={enchantedSellPrice}>
+        <button class="btn btn-solid" on:click={updateProduct}>Update</button>
 
-        <p>Amount Bought of Normal</p>
-        <input bind:value={initalPurchaseAmount}>
-
-        <p>Tax Rate</p>
-        <input bind:value={taxRate}>
-
-        <p>Profit Per Item: {profitPerItem}</p>
-        <p>Total Profit: {totalProfit}</p>
+        {#key product}
+            <svelte:component this="{BazaarItem}" name={productName} dataObject={product} {products} {items} {productMap}/>
+        {/key}
     </div>
 </div>
 
@@ -37,5 +66,31 @@
     .app {
         margin: auto;
         text-align: center;
+    }
+
+    h1 {
+        padding: 1rem;
+    }
+
+    input {
+        margin: 1rem 0;
+
+        border: solid 1.5px $primary;
+        appearance: none;
+
+        font-size: 0.83rem;
+		color: $black;
+		font-family: Inter;
+		font-weight: bold;
+
+		align-items: center;
+		justify-content: center;
+		position: relative;
+		padding: 10px 15px;
+
+		border-radius: 4px;
+
+		transition: all 0.5s;
+		box-shadow: 0px 3px 6px 0px rgba($color: #000000, $alpha: 0.16);
     }
 </style>
