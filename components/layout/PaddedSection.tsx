ee848e1d30e4acc4e9d33af4a9a66f13fea05825
IlluzionzDev <jaminstratford@gmail.com>
2022-05-21 15:00:01 +1200

    Define type for component

--- components/layout/PaddedSection.tsx
+++ components/layout/PaddedSection.tsx
@@ -1,6 +1,6 @@
 import styles from "./PaddedSection.module.scss";
 
-const PaddedSection = ({ children }) => (
+const PaddedSection: React.FC = ({ children }) => (
   <section className={`container ${styles.section}`}>
     {children}
   </section>
