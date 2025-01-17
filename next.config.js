f2d4f02037bff1c68980bd135a78072cb919891d
IlluzionzDev <jaminstratford@gmail.com>
2025-01-18 12:39:20 +1300

    Update packages and structure for rebuild

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 const nextConfig = {
     reactStrictMode: true,
-    compiler: {
-        styledComponents: true,
-    },
     output: 'standalone',
-    sassOptions: {
-        includePaths: ['./styles', './components'],
-        prependData: `@import "@illuzionz-studios/design-system/style-utils";`,
-    },
-    i18n: {
-        locales: ['en'],
-        defaultLocale: 'en',
-    },
 };
 
 module.exports = nextConfig;
